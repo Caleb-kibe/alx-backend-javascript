7e16638bdb57d2d021d87e75ae0491773f11d812
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-17 15:06:45 +0300

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+// Reading a file asynchronously
+
+const { rejects } = require('assert');
+const { resolve } = require('path');
+
+const fs = require('fs').promises;
+
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8')
+        .then((data) => {
+            const lines = data.split('\n').filter(line => line.trim() !== '');
+            const students = lines.slice(1);
+
+            console.log('Number of students: ${students.length}');
+
+            const fieldCounts = {};
+
+            students.forEach(student => {
+                const [firstname, lastname, age, field] = student.split(',');
+                if (!fieldCounts[field]) {
+                    fieldCounts[field] = [];
+                }
+                fieldCounts[field].push('firstname');
+            });
+
+            for (const [field, students] of Object.entries(fieldCounts)) {
+                console.log('Number of Students in ${field}: ${students.length}. List: ${students.join(', ')}');
+            }
+            resolve();
+        })
+        .catch(() => {
+            reject(new Error('Cannot load the database'));
+        });
+    });
+}
+
+module.exports = countStudents;
